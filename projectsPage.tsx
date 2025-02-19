74862b7b411579efeed3e15451e40d4cb55782fb
Michel D <150844280+michel-DC@users.noreply.github.com>
2025-02-19 14:07:30 +0100

    Update projectsPage.tsx

--- projectsPage.tsx
+++ projectsPage.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 
-export const Projects  = () => {
+export const Projects = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const totalSlides = 2; // Update this to the total number of slides
 
@@ -19,20 +19,12 @@ export const Projects  = () => {
       <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0 font-caption">
         Projects
       </h2>
-      <button
-        className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground fixed h-8 w-8 rounded-full right-4 top-1/2 -translate-y-1/2"
-        onClick={handleNext}
-        disabled={currentSlide === totalSlides - 1}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right h-4 w-4">
-          <path d="M5 12h14" />
-          <path d="M12 19l7-7-7-7" />
-        </svg>
-        <span className="sr-only">Next slide</span>
-      </button>
+      <p>
+          Take a look at my selection of <b>favorite</b> projects.
+      </p>
       <div className="relative mt-8 max-lg:mx-6" role="region" aria-roledescription="carousel">
         <button
-          className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground fixed h-8 w-8 rounded-full left-4 top-1/2 -translate-y-1/2"
+          className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground absolute h-8 w-8 rounded-full -left-12 top-1/2 -translate-y-1/2"
           onClick={handlePrev}
           disabled={currentSlide === 0}
         >
@@ -41,6 +33,17 @@ export const Projects  = () => {
             <path d="M19 12H5" />
           </svg>
           <span className="sr-only">Previous slide</span>
+        </button>
+        <button
+          className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground absolute h-8 w-8 rounded-full -right-12 top-1/2 -translate-y-1/2"
+          onClick={handleNext}
+          disabled={currentSlide === totalSlides - 1}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right h-4 w-4">
+            <path d="M5 12h14" />
+            <path d="M12 19l7-7-7-7" />
+          </svg>
+          <span className="sr-only">Next slide</span>
         </button>
         <div className="overflow-hidden">
           <div className="flex -ml-4" style={{ transform: `translateX(-${currentSlide * 100}%)`, transition: 'transform 0.5s ease' }}>
@@ -77,7 +80,7 @@ export const Projects  = () => {
                     </div>
                   </div>
                 </div>
-                </a>
+              </a>
             </div>
 
             {/* Slide 3 */}
