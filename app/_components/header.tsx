ce98760abbd125a14ef423d3b801744ed8736a69
Michel <djoumessi.michelc@gmail.com>
2025-02-20 08:06:06 +0100

    Update Header component: adjust height and margin for improved layout and responsiveness

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,9 +3,9 @@ import { Section } from "./ui/section";
 
 export const Header = () => {
   return (
-    <Section className="sticky top-0 mb-14 mx-auto flex w-full max-w-4xl h-80px items-center justify-between px-4 backdrop-blur-xl rounded-md">
+    <Section className="sticky top-0 mb-14 mx-auto flex w-full max-w-4xl h-20 items-center justify-between px-4 backdrop-blur-xl rounded-md">
       <div className="flex items-center gap-1">
-        <h1 className="text-primary-foreground font-sans font-extrabold mr-96">
+        <h1 className="text-primary-foreground font-sans font-extrabold mr-4 md:mr-96">
           michel.dev
         </h1>
       </div>
@@ -14,7 +14,7 @@ export const Header = () => {
           style={{ opacity: 1 }}
           className="flex items-center gap-4 text-sm font-medium text-muted-foreground"
         >
-          <ul className="hover:cursor-pointer flex gap-4 justify-end">
+          <ul className="hover:cursor-pointer flex gap-4 justify-end flex-wrap">
             <li>About</li>
             <li>Skills</li>
             <li>Projects</li>
