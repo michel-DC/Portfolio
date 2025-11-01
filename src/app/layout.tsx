31e34e97e6fdd678cf5ec1bb33c4ca2abcc1ce58
michel-DC <djoumessi.michelc@gmail.com>
2025-11-01 15:54:33 +0100

    Add Google Tag Manager integration for enhanced tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { Background } from "@/components/background/background";
+import Script from "next/script";
 
 const bricolageGrotesque = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -87,6 +88,13 @@ export default function RootLayout({
       className={`h-full text-foreground ${bricolageGrotesque.variable} ${playfair.variable}`}
     >
       <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5QMDFMGS');`}
+        </Script>
         <link
           rel="shortcut icon"
           href="/images/logo/favicon.png"
@@ -117,6 +125,14 @@ export default function RootLayout({
         />
       </head>
       <body className="font-bricolage-grotesque h-full">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QMDFMGS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Background>
           <Header />
           {children}
